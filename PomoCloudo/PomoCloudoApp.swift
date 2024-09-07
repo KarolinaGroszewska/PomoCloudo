58eaece0dc17b7045d3ace241b8913cb4d7bfb43
Karolina Groszewska <karolina.groszewska@gmail.com>
2024-09-07 09:48:21 -0500

    Initial Commit

---
+++ PomoCloudo/PomoCloudoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoCloudoApp.swift
+//  PomoCloudo
+//
+//  Created by Kari Groszewska on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PomoCloudoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
