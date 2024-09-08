7bae11715eb4c411b2437f094d45db2d2c029565
Karolina Groszewska <karolina.groszewska@gmail.com>
2024-09-08 15:48:54 -0500

    part 2 stream progress!

--- PomoCloudo/ContentView.swift
+++ PomoCloudo/ContentView.swift
@@ -8,17 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var timeRemaining = 240
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(red: 255/255, green: 242/255, blue: 242/255)
+                .ignoresSafeArea()
+            Text(convertSecondsToTime(timeInSeconds: timeRemaining))
+                .foregroundStyle(Color(red: 71/255, green: 21/255, blue: 21/255))
+                .font(.variableFont(210.92, axis: [.weight: 690]))
+                .environment(\._lineHeightMultiple, 0.78)
+                .onReceive(timer) { _ in
+                    timeRemaining -= 1
+                }
         }
-        .padding()
+    }
+    
+    func convertSecondsToTime(timeInSeconds: Int) -> String {
+        let minutes = timeInSeconds / 60
+        let seconds = timeInSeconds % 60
+        return (String(format: "%02i\n%02i", minutes, seconds))
     }
 }
+
 
 #Preview {
     ContentView()
 }
+
