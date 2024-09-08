1efc555294bc479fc667c006f69a72ba52db0165
Karolina Groszewska <karolina.groszewska@gmail.com>
2024-09-08 18:38:25 -0500

    part 3 progress

--- PomoCloudo/ColorSchemes.swift
+++ PomoCloudo/ColorSchemes.swift
@@ -5,4 +5,16 @@
 //  Created by Kari Groszewska on 9/8/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    static let focusText = Color(red: 71/255, green: 21/255, blue: 21/255)
+    static let focusBackground = Color(red: 255/255, green: 242/255, blue: 242/255)
+    static let focusPrimaryButtonBackground = Color(red: 255/255, green: 76/255, blue: 76/255, opacity: 0.15)
+    static let focusSecondaryButtonBackground = Color(red: 255/255, green: 76/255, blue: 76/255, opacity: 0.71)
+    
+    static let breakText = Color(red: 20/255, green: 64/255, blue: 29/255)
+    static let breakBackground = Color(red: 242/255, green: 255/255, blue: 245/255)
+    static let breakPrimaryButtonBackground = Color(red: 77/255, green: 218/255, blue: 110/255, opacity: 0.15)
+    static let breakSecondaryButtonBackground = Color(red: 77/255, green: 218/255, blue: 110/255, opacity: 0.62)
+}
--- PomoCloudo/ContentView.swift
+++ PomoCloudo/ContentView.swift
@@ -8,20 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var timeRemaining = 240
+    @State var workingSession = true
+    @State var timerOn = false
+    //TODO: Override Pomodoro Length with User Preferences
+    @State var timeRemaining = 1500
+    @State var timeInFocus = 0
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack{
-            Color(red: 255/255, green: 242/255, blue: 242/255)
-                .ignoresSafeArea()
-            Text(convertSecondsToTime(timeInSeconds: timeRemaining))
-                .foregroundStyle(Color(red: 71/255, green: 21/255, blue: 21/255))
-                .font(.variableFont(210.92, axis: [.weight: 690]))
-                .environment(\._lineHeightMultiple, 0.78)
-                .onReceive(timer) { _ in
-                    timeRemaining -= 1
+            workingSession ? Color.focusBackground .ignoresSafeArea() : Color.breakBackground .ignoresSafeArea()
+               
+            VStack{
+                Spacer()
+                Button(action: {
+                    timeRemaining = workingSession ? 1500 : 300
+                    timerOn = true
+                }, label: {
+                    HStack{
+                        Text(workingSession ?  Image(systemName: "brain") : Image(systemName: "cup.and.saucer"))
+                        Text(workingSession ? "Focus" : "Short Break")
+                    }
+                    .foregroundStyle(workingSession ? Color.focusText : Color.breakText)
+                    .padding([.top, .bottom], 6)
+                    .padding([.leading, .trailing], 10)
+                })
+                .background(workingSession ? Color.focusPrimaryButtonBackground : Color.breakPrimaryButtonBackground)
+                .clipShape(Capsule())
+                .overlay(
+                    RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                        .stroke(workingSession ? Color.focusText : Color.breakText)
+                )
+                .padding(.bottom, -30)
+
+                Text(convertSecondsToTime(timeInSeconds: timeRemaining))
+                    .foregroundStyle(workingSession ? Color.focusText : Color.breakText)
+                    .font(.variableFont(210.92, axis: [.weight: 690]))
+                    .environment(\._lineHeightMultiple, 0.78)
+                    .onReceive(timer) { _ in
+                        if timerOn {
+                            timeRemaining -= 1
+                        }
+                    }
+                HStack{
+                    Button {
+                        //TODO: Open up a settings menu
+                    } label: {
+                        Text(Image(systemName: "ellipsis"))
+                            .font(.variableFont(25))
+                            .foregroundStyle(workingSession ? Color.focusText : Color.breakText)
+                            .padding(18)
+                            .background(workingSession ? Color.focusPrimaryButtonBackground : Color.breakPrimaryButtonBackground)
+                            .clipShape(.buttonBorder)
+
+                    }
+                    Button {
+                        timerOn.toggle()
+                    } label: {
+                        Text(Image(systemName: timerOn ? "pause.fill": "play.fill" ))
+                            .font(.variableFont(25))
+                            .foregroundStyle(workingSession ? Color.focusText : Color.breakText)
+                            .padding(25)
+                            .padding([.leading, .trailing], 15)
+                            .background(workingSession ? Color.focusSecondaryButtonBackground : Color.breakSecondaryButtonBackground)
+                            .clipShape(.buttonBorder)
+                    }
+                    Button {
+                        workingSession.toggle()
+                        timeRemaining = workingSession ? 1500 : 300
+                        timerOn = false
+                    } label: {
+                        Text(Image(systemName: "forward.fill"))
+                            .font(.variableFont(25))
+                            .foregroundStyle(workingSession ? Color.focusText : Color.breakText)
+                            .padding(18)
+                            .background(workingSession ? Color.focusPrimaryButtonBackground : Color.breakPrimaryButtonBackground)
+                            .clipShape(.buttonBorder)
+
+                    }
+                    
                 }
+                .padding(.top, -120)
+                Spacer()
+            }
         }
     }
     
